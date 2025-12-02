e903f8a4d91cb4a1bcb6b06a28e9f5f66f891620
YAJE <gilmiercabil@gmail.com>
2025-12-02 14:09:08 +0800

    u

--- Day5.js
+++ Day5.js
@@ -1,8 +1,18 @@
 // Practice Fetch API
 
+const list = document.getElementById("pokemon");
+const insert = document.getElementById("input1");
+
 function getPokemon() {
   fetch("config.json")
     .then((res) => res.json())
-    .then((data) => console.log(data.name))
+    .then((data) => {
+      const result = data.find((e) => e.id === insert.value);
+      if (result) {
+        list.innerText = data.map((e) => e.name);
+      } else {
+        list.innerText = "not on the list";
+      }
+    })
     .catch((error) => console.log(error));
 }
