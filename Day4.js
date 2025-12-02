c03428c3923f77e3e623791a9feac6bb67ff8d16
YAJE <gilmiercabil@gmail.com>
2025-12-02 11:58:01 +0800

    Update Day4.js

--- Day4.js
+++ Day4.js
@@ -4,22 +4,15 @@ const insert3 = document.getElementById("merge3");
 const insert4 = document.getElementById("merge4");
 
 // Merge Two Arrays
-const room1 = [
-  { name: "james", age: 13 },
-  { name: "ashley", age: 14 },
-];
+const room1 = ["james", " trones"];
 
-const room2 = [
-  { name: "jamess", age: 13 },
-  { name: "ashleys", age: 14 },
-];
+const room2 = [" undertaker", " kolots"];
 
 // Merge arrays
 const mix = [...room1, ...room2];
 
 // Display (convert to text first)
-insert1.innerText = JSON.stringify(mix, null, 2);
- 
+insert1.innerText = `${mix}`;
 
 // Copy an Object
 const student = { name: "Gilmier", age: 19 };
@@ -28,8 +21,8 @@ const studentCopy = { ...student };
 insert2.innerText = JSON.stringify(studentCopy, null, 2);
 
 // Add New Values Without Changing the Original
-const numbers = [1, 2, 3];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const newNumbers = [...numbers, 4, 5];
+const newNumbers = [numbers.find((e) => e == 7), 11, 12];
 insert3.innerText = numbers;
-insert4.innerText = newNumbers;
+insert4.innerText = newNumbers.reduce((f, l) => f + l);
