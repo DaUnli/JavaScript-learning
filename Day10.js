d0c6c84ad3ccf6ad938171b6884febd3531ab088
YAJE <gilmiercabil@gmail.com>
2025-12-10 19:31:48 +0800

    y

--- Day10.js
+++ Day10.js
@@ -1,5 +1,5 @@
 const SaveName = () => {
-  const savedName = localStorage.getItem("username");
+  const savedName = localStorage.getItem("uername");
   if (savedName) {
     document.getElementById("display").innerText =
       "Saved Username: " + savedName;
@@ -10,9 +10,8 @@ const SaveName = () => {
 window.onload = SaveName();
 
 // 2. Save username when clicking the button
-document.getElementById("saveBtn").onclick = () => {
+document.getElementById("saveBtn").addEventListener("click", () => {
   const name = document.getElementById("username").value;
   localStorage.setItem("username", name);
   document.getElementById("display").innerText = "Saved Username: " + name;
-};
-  
+});
